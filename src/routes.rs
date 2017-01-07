bb441dce6a2439f78042dce08c1b0341f0079469
SilverWingedSeraph <lfstindall@gmail.com>
2017-01-07 15:39:35 -0800

    Add JSON quotes list endpoint.
    This endpoint is at /json/all and is in accordance with issue #5

--- src/routes.rs
+++ src/routes.rs
@@ -88,3 +88,9 @@ pub fn json() -> JSON<String> {
     let quote = quotes::get_random_quote().unwrap();
     JSON(serde_json::to_string(&quote).unwrap())
 }
+
+#[get("/json/all")]
+pub fn json_all() -> JSON<String> {
+    let quotes = quotes::get_quotes().unwrap();
+    JSON(serde_json::to_string(&quotes).unwrap())
+}
